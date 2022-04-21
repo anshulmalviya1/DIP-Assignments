8e32204686d2c501f82bbb5c7fa3e07f42cdaf61
Anshul Malviya <88474736+anshulmalviya1@users.noreply.github.com>
2022-04-21 12:22:31 +0530

    Update Assignment9.py

--- Assignment9/Assignment9.py
+++ Assignment9/Assignment9.py
@@ -4,25 +4,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 	
-# function to obtain histogram of an image
+
 def hist_plot(img):
-	
-	# empty list to store the count
-	# of each intensity value
+
 	count =[]
-	
-	# empty list to store intensity
-	# value
 	r = []
 	
-	# loop to traverse each intensity
-	# value
+
 	for k in range(0, 256):
 		r.append(k)
 		count1 = 0
 		
-		# loops to traverse each pixel in
-		# the image
 		for i in range(m):
 			for j in range(n):
 				if img[i, j]== k:
@@ -34,8 +26,7 @@ def hist_plot(img):
 
 img = cv2.imread('img.jpeg', 0)
 
-# To ascertain total numbers of rows and
-# columns of the image, size of the image
+
 m, n = img.shape
 r1, count1 = hist_plot(img)
 
